f8fe8b6c97a65e74a23f8130ed63028ea43178ac
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-07-22 18:44:14 -0700

    first commit

---
+++ src/components/TodoForms.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function TodoForms(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
+
+  const handleChange = e => {
+      setInput(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onsubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput('')
+  };
+  return (
+    <div>
+      <form className="todo-form" onSubmit={handleSubmit}>
+
+        {props.edit ? ( 
+          <>
+          <input
+          type='text'
+          placeholder='Update your item'
+          value={input}
+          name='text'
+          className='todo-input edit'
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <button className="todo-button edit">Update</button>
+        </>
+        ) : 
+        
+        ( 
+          <>
+          <input
+          type="text" autoComplete="off"
+          placeholder="Add a todo"
+          value={input}
+          name="text"
+          className="todo-input"
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <button className="todo-button">Add todo</button>
+        </>
+        )}
+      
+      </form>
+    </div>
+  );
+}
+export default TodoForms;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as TodoForms} from './TodoForms.js'
+export { default as TodoList} from './TodoList.js'
